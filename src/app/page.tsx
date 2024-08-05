0f2dbda39258f8fd069c73cb09c668d1306f3e5c
parlamas <parlamas@live.com>
2024-08-05 15:31:00 +0200

    ag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
-import Image from "next/image";
-import Head from 'next/head';
-import './globals.css'; 
+// src/app/page.tsx
+import './globals.css';
+
+export const metadata = {
+  title: 'Dialectics Center',
+};
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Dialectics Center</title>
-      </Head>
-      <div className="flex items-center justify-center h-full">
-        <div className="text-2xl" style={{ backgroundColor: "#2b6304", color: "yellow" }}>
-          Dialectics Center
-        </div>
+    <div className="flex items-center justify-center h-full">
+      <div className="text-2xl" style={{ backgroundColor: "#2b6304", color: "yellow" }}>
+        Dialectics Center
       </div>
-    </>
+    </div>
   );
 }
