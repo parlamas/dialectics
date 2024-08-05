9627f42faf65ca28bbb7c706e0544b21a1417863
parlamas <parlamas@live.com>
2024-08-05 15:18:29 +0200

    af

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Head from 'next/head';
+import './globals.css'; 
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="text-2xl" style={{background-color: "beige"; color:'blue'}}>Dialectics Center</div>
-    </div>
+    <>
+      <Head>
+        <title>Dialectics Center</title>
+      </Head>
+      <div className="flex items-center justify-center h-full">
+        <div className="text-2xl" style={{ backgroundColor: "#2b6304", color: "yellow" }}>
+          Dialectics Center
+        </div>
+      </div>
+    </>
   );
 }
