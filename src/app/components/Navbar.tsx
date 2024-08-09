b18559efe699ae56b3f07d686815dacbc5d810d0
parlamas <parlamas@live.com>
2024-08-09 07:10:01 +0200

    be

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,12 +11,8 @@ const Navbar: React.FC = () => {
   const { userId } = useAuth();
   const router = useRouter();
   const [menuOpen, setMenuOpen] = useState(false);
-  const [openSubMenu, setOpenSubMenu] = useState<string | null>(null); // State to track the open submenu
-  const [formOpen, setFormOpen] = useState(false); // State to track the form visibility
-  const [formData, setFormData] = useState({ name: '', email: '', request: '' });
-  const [message, setMessage] = useState<string | null>(null); // State to track success message
+  const [openSubMenu, setOpenSubMenu] = useState<string | null>(null);
   const menuRef = useRef<HTMLDivElement>(null);
-  const formRef = useRef<HTMLDivElement>(null);
 
   const philosophyItems = [
     { href: "/philosophy/dialectics", text: "Dialectics" },
@@ -81,56 +77,20 @@ const Navbar: React.FC = () => {
 
   const handleSubMenuClick = (label: string) => {
     setOpenSubMenu(openSubMenu === label ? null : label);
-    setFormOpen(false); // Close the form when opening a submenu
-    setMessage(null); // Clear message when submenu is clicked
   };
 
-  const handleFormClick = () => {
-    setFormOpen(!formOpen);
-    setOpenSubMenu(null); // Close the submenu when opening the form
-    setMessage(null); // Clear message when form is opened
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('/api/send-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        setMessage('You emailed successfully.');
-        setFormData({ name: '', email: '', request: '' }); // Clear form
-      } else {
-        setMessage('Failed to send email.');
-      }
-    } catch (error) {
-      console.error('Error sending email:', error);
-      setMessage('An error occurred while sending the email.');
-    }
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+    setOpenSubMenu(null);
   };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
-        menuRef.current && !menuRef.current.contains(event.target as Node) &&
-        (!formRef.current || !formRef.current.contains(event.target as Node))
+        menuRef.current && !menuRef.current.contains(event.target as Node)
       ) {
         setMenuOpen(false);
         setOpenSubMenu(null);
-        setFormOpen(false);
       }
     };
 
@@ -140,18 +100,39 @@ const Navbar: React.FC = () => {
     };
   }, []);
 
-  const handleMenuClick = () => {
-    setMenuOpen(!menuOpen);
-    setOpenSubMenu(null);
-    setFormOpen(false);
-  };
-
   return (
     <header className="navbar bg-gray-800 text-white fixed w-full top-0 left-0 z-40">
       <div ref={menuRef} className="container mx-auto px-4 py-3 flex items-center justify-between">
         <Link href="/" className="text-lg font-semibold menu-item">
           <span>Home</span>
         </Link>
+        <nav className="hidden md:flex space-x-4">
+          <SubMenu items={philosophyItems} label="Philosophy" />
+          <SubMenu items={historyItems} label="History" />
+          <SubMenu items={musicItems} label="Music" />
+          <SubMenu items={physicalEducationItems} label="Physical Education" />
+          <SubMenu items={socialSkillsItems} label="Social Skills" />
+          <SubMenu items={languagesItems} label="Languages" />
+          <SubMenu items={physicsItems} label="Physics" />
+          <SubMenu items={mathItems} label="Math" />
+          {!userId ? (
+            <div className="flex space-x-4">
+              <Link href="/sign-in" className="block px-4 py-2 hover:text-gray-300 menu-item">
+                <span>Sign in</span>
+              </Link>
+              <Link href="/sign-up" className="block px-4 py-2 hover:text-gray-300 menu-item">
+                <span>Sign up</span>
+              </Link>
+            </div>
+          ) : (
+            <div className="flex space-x-4 items-center">
+              <Link href="/profile" className="block px-4 py-2 hover:text-gray-300 menu-item">
+                Profile
+              </Link>
+              <UserButton />
+            </div>
+          )}
+        </nav>
         <button
           className="md:hidden text-white"
           onClick={handleMenuClick}
@@ -160,84 +141,36 @@ const Navbar: React.FC = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
-        <nav className={`md:flex ${menuOpen ? 'block' : 'hidden'} md:space-x-4 absolute md:relative top-full left-0 w-full md:w-auto bg-gray-800 md:bg-transparent`}>
-          <div className="menu-container">
-            <button onClick={handleFormClick} className="block px-4 py-2 hover:text-gray-300 menu-item">
-              <span>Content Request</span>
-            </button>
-            <SubMenu items={philosophyItems} label="Philosophy" isOpen={openSubMenu === 'Philosophy'} onClick={() => handleSubMenuClick('Philosophy')} />
-            <SubMenu items={historyItems} label="History" isOpen={openSubMenu === 'History'} onClick={() => handleSubMenuClick('History')} />
-            <SubMenu items={musicItems} label="Music" isOpen={openSubMenu === 'Music'} onClick={() => handleSubMenuClick('Music')} />
-            <SubMenu items={physicalEducationItems} label="Physical Education" isOpen={openSubMenu === 'Physical Education'} onClick={() => handleSubMenuClick('Physical Education')} />
-            <SubMenu items={socialSkillsItems} label="Social Skills" isOpen={openSubMenu === 'Social Skills'} onClick={() => handleSubMenuClick('Social Skills')} />
-            <SubMenu items={languagesItems} label="Languages" isOpen={openSubMenu === 'Languages'} onClick={() => handleSubMenuClick('Languages')} />
-            <SubMenu items={physicsItems} label="Physics" isOpen={openSubMenu === 'Physics'} onClick={() => handleSubMenuClick('Physics')} />
-            <SubMenu items={mathItems} label="Math" isOpen={openSubMenu === 'Math'} onClick={() => handleSubMenuClick('Math')} />
-            {!userId ? (
-              <div className="flex space-x-4">
-                <Link href="/sign-in" className="block px-4 py-2 hover:text-gray-300 menu-item">
-                  <span>Sign in</span>
-                </Link>
-                <Link href="/sign-up" className="block px-4 py-2 hover:text-gray-300 menu-item">
-                  <span>Sign up</span>
-                </Link>
-              </div>
-            ) : (
-              <div className="flex space-x-4 items-center">
-                <Link href="/profile" className="block px-4 py-2 hover:text-gray-300 menu-item">
-                  Profile
-                </Link>
-                <UserButton />
-              </div>
-            )}
-          </div>
-        </nav>
       </div>
-      {formOpen && (
-        <div ref={formRef} className="form-container absolute top-16 left-0 right-0 bg-white text-black p-4 shadow-lg rounded mx-4 md:mx-auto md:w-1/2">
-          <h2>Content Request Form</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="name" className="block text-gray-700">Name</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleInputChange}
-                className="w-full px-3 py-2 border rounded"
-                required
-              />
+      <div className={`navbar-menu ${menuOpen ? 'active' : ''} md:hidden`}>
+        <div className="menu-container">
+          <button onClick={() => handleSubMenuClick('Philosophy')} className="block px-4 py-2 hover:text-gray-300 menu-item">
+            Philosophy
+          </button>
+          {openSubMenu === 'Philosophy' && (
+            <div className="submenu-container show">
+              {philosophyItems.map((item, index) => (
+                <Link key={index} href={item.href} className="block px-4 py-2">
+                  {item.text}
+                </Link>
+              ))}
             </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-gray-700">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                className="w-full px-3 py-2 border rounded"
-                required
-              />
+          )}
+          <button onClick={() => handleSubMenuClick('History')} className="block px-4 py-2 hover:text-gray-300 menu-item">
+            History
+          </button>
+          {openSubMenu === 'History' && (
+            <div className="submenu-container show">
+              {historyItems.map((item, index) => (
+                <Link key={index} href={item.href} className="block px-4 py-2">
+                  {item.text}
+                </Link>
+              ))}
             </div>
-            <div className="mb-4">
-              <label htmlFor="request" className="block text-gray-700">Request</label>
-              <textarea
-                id="request"
-                name="request"
-                value={formData.request}
-                onChange={handleInputChange}
-                className="w-full px-3 py-2 border rounded"
-                required
-              ></textarea>
-            </div>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Submit</button>
-            <button type="button" className="ml-4 text-red-500 px-4 py-2 rounded hover:text-red-700" onClick={() => setFormOpen(false)}>Close Form</button>
-          </form>
-          {message && <p className="mt-4 text-green-500">{message}</p>}
+          )}
+          {/* Repeat for other submenus */}
         </div>
-      )}
+      </div>
     </header>
   );
 };
